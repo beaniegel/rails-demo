4b5268c11250aef7a8abc5f6568e95166d456519
jellybean <mail+github@jelena.io>
2018-11-23 16:06:39 +0000

    added maths section

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -1,7 +1,13 @@
 class SayController < ApplicationController
   def hello
     @time = Time.now
-    @timein1hour = 1.hour.from_now.localtime 
+    @timein1hour = 1.hour.from_now.localtime
+    @n1 = 184
+    @n2 = 293
+    @n_sum = @n1 + @n2
+    @s1 = "I can also "
+    @s2 = "do maths"
+    @message = @s1 + @s2
   end
 
   def goodbye
